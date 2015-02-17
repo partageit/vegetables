6cd1e4be2baa16c104102bc7d14acad5e8f2796f
David Duquenoy <david@partage-it.com>
2015-02-17 06:21:47 +0100

    Optimist replaced with yargs and clean up in index.js
    Parameters --serve and --generate hyphens removed, changed as vegetables serve
    Usage and examples added to CLI
    version.js and help.js are not required anymore

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 #!/usr/bin/env node
 //var path = require('path');
 //var templateDir = __dirname + '/template';
-var optimist = require('optimist')
+var pkg = require('./package');
+var yargs = require('yargs')
+.usage(pkg.name + ' [options] <command>')  // pkg.name may be replaced with '$0'
+.command('generate', 'Generate static Web pages from current folder Markdown documents')
+.command('serve', 'Serve and watch the generated static Web pages')
+.example(pkg.name + ' generate -t ../my-template', 'Use the provided template folder')
+.example(pkg.name + ' serve -p 8989', 'Serve the static pages at http://localhost:8989')
+.epilog(pkg.name + ' v' + pkg.version + ' - For more informations, check out our site: http://???')
 .alias({
-	//	't': 'title',
-	//	'l': 'template',
-	//	's': 'style',
-	//	'j': 'script',
+	//	't': 'template',
 	'h': 'help',
-	//	'o': 'output-file',
-	//	'w': 'watch',
 	'v': 'version',
-	//	'd': 'document-mode'
-	's': 'serve',
-	'g': 'generate'
 })
 .describe({
-	//	'title': 'Generated page title',
-	//	'style': 'Path to custom stylesheet',
-	//	'script': 'Path to custom javascript',
+	//	'title': 'Generated Web site title',
 	//	'template': 'Path to custom mustache template',
+	// port, host, config, ...
 	'help': 'This screen',
-	//	'output-file': 'Path to output file (stdout if not specified)',
-	//	'document-mode': 'Generate slides from a document without slide separators (---) or annotations',
-	//	'watch': 'Watch mode'
-	'serve': 'Serve Markdown document as Web pages',
-	'generate': 'Generate static Web pages'
+	'version': 'Display the... version'
 })
-.boolean('serve')
-.boolean('generate')
-.boolean('version');
+.boolean('version')
+.boolean('help')
+.string('_');
 //.default({
-//	'style': path.resolve(templateDir + '/style.css'),
-//	'template': path.resolve(templateDir + '/template.html')
+//	'config': 'vegetables.json' // should not begin with .
 //});
-var argv = optimist.argv;
+var argv = yargs.argv;
 if (argv.v) {
-	console.log(require('./lib/version-string')());
-	//} else if (argv.h) {
-	//	console.log(require('./lib/help')(optimist));
-} else if (argv.s) {
+	console.log(pkg.name + ' v' + pkg.version);
+} else if (argv.h) {
+	//console.log(require('./lib/help')(yargs));
+	console.log(yargs.help());
+} else if (argv._[0] === 'serve') {
 	require('./lib/serve')(argv);
-} else if (argv.g) {
+} else if (argv._[0] === 'generate') {
 	require('./lib/generate')(argv, true);
 } else {
-	console.log(require('./lib/help')(optimist));
+	console.log(yargs.help());
 }
